2ed0598c50d29568b38ced658af86a2008fe365b
sm <a0086845@u.nus.edu>
2017-10-10 19:16:57 +0800

    car test

--- src/carFn.js
+++ src/carFn.js
@@ -1,6 +1,25 @@
 // Phase I, don't require this file after you're done with Phase I
 
 // Create the object properties and function
-var Car = {}
+var Car = {
+  make: 'Honda',
+  model: 'Vuzel',
+  year: 2017,
+  color: 'red',
+  seats: 7,
+  previousOwners: [],
+  owner: 'manufacturer',
+  running: false,
+
+  sell(newOwner) {
+    previousOwners.push(owner);
+    owner = newOwner;
+  },
+
+  paint(newColor) {
+    color = newColor;
+  }
+};
 
 // Export the `Car` object
+module.exports = Car;
