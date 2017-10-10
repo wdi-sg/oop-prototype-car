343237970cb27707117ed2027f4a2a43ab71ec95
subutai1175 <subutai1175@gmail.com>
2017-10-10 19:11:49 +0800

    phase III done

--- src/Car.js
+++ src/Car.js
@@ -1,4 +1,5 @@
 // Phase II here, don't require this file until you're done with Phase I
+// require('./carFn');
 
 class Car {
   constructor(make, model, year, color, seats) {
@@ -6,14 +7,22 @@ class Car {
     this.model = model
     this.year = year
     // TODO: add color, seats here
-  }
-
+    this.color = color
+    this.seats = seats
+    this.previousOwner = []
+    this.owner = 'manufacturer'
+    this.running = false
+    }
   // add the sell function
-
-  // add the paint function
-
-
+    sell(newOwner) {
+      this.previousOwner.push(this.owner);
+      this.owner = newOwner;
+    }
+    paint(newColor) {
+      this.color = newColor;
+    }
 }
 
 // export the Car class //
 // this is required for the carTest.js to load this //
+module.exports = Car
--- test/carTest.js
+++ test/carTest.js
@@ -7,7 +7,7 @@ var success = require('./helpers/success')
 // load the Car object for
 var Car = require('../src/Car')
 // update the car instantiation below according to the test given
-var acura = new Car('Honda', 'Vuzel', 2017, 'Blue', 7)
+var acura = new Car('Acura', 'Integra', 1999, 'Red', 4)
 
 
 // starter code - testing constructor
@@ -22,7 +22,31 @@ assert.strictEqual(acura.seats, 4, 'Constructor did not set seats.')
 success()
 
 // test sell
-console.log('Testing selling a car')
+// console.log('Testing selling a car')
 acura.sell('Lenny')
 
 // You're on your own from here
+
+var c1 = new Car('Honda', 'Vuzel', 2017, 'red', 7)
+
+assert.strictEqual(c1.make, 'Honda', 'Constructor did not set make.')
+assert.strictEqual(c1.model, 'Vuzel', 'Constructor did not set model.')
+assert.strictEqual(c1.year, 2017, 'Constructor did not set year.')
+assert.strictEqual(c1.color, 'red', 'Constructor did not set color.')
+assert.strictEqual(c1.seats, 7, 'Constructor did not set seats.')
+assert.strictEqual(c1.owner, 'manufacturer', 'Constructor did not set owner')
+assert.strictEqual(c1.running, false, 'Constructor did not set running')
+c1.sell('prima')
+assert.strictEqual(c1.owner, 'prima', 'Constructor did set owner.')
+// console.log(c1.previousOwner)
+assert.strictEqual(c1.previousOwner[0], 'manufacturer', 'Constructor did not set previous owner')
+c1.paint('blue')
+assert.strictEqual(c1.color, 'blue', 'Constructor did not set paint method to blue')
+
+c1.sell(0)
+assert.strictEqual((typeof c1.owner === 'string'), false, 'car can only be sold to a real person with name, please input a string')
+
+c1.paint(false)
+assert.strictEqual((typeof c1.color === 'string'), false, 'car can only be painted with real color, please input a string')
+
+success();
