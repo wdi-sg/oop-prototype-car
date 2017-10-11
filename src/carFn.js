14ce7000adfe8e8e92e71552e4e375a4da4cdc26
Matthew Francis Ong <matthewfoys@gmail.com>
2017-10-11 11:57:01 +0800

    submit homework

--- src/carFn.js
+++ src/carFn.js
@@ -1,6 +1,25 @@
 // Phase I, don't require this file after you're done with Phase I
 
 // Create the object properties and function
-var Car = {}
+var Car = {
+  make:'Honda',
+  model: 'Vuzel',
+  year: 2017,
+  color: 'red',
+  seats: 7,
+  previousOwners: [],
+  owner: "manufacturers",
+  running: false,
+  sell: function(newOwner) {
+    this.previousOwners.push(this.owner)
+    return this.owner = newOwner
+  },
+  paint: function(newColor) {
+    return this.color = newColor
+  },
+}
+
+module.exports = Car
+
 
 // Export the `Car` object
