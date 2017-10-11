174b55402aa78c0fd40e599a0aeeb0c3c9489e9d
subutai1175 <subutai1175@gmail.com>
2017-10-11 11:56:48 +0800

    submit assignment

--- src/Car.js
+++ src/Car.js
@@ -2,7 +2,7 @@
 // require('./carFn');
 
 class Car {
-  constructor(make, model, year, color, seats) {
+  constructor(make, model, year, color, seats, passengers) {
     this.make = make
     this.model = model
     this.year = year
@@ -12,6 +12,7 @@ class Car {
     this.previousOwner = []
     this.owner = 'manufacturer'
     this.running = false
+    this.passengers = []
     }
   // add the sell function
     sell(newOwner) {
@@ -21,8 +22,35 @@ class Car {
     paint(newColor) {
       this.color = newColor;
     }
-}
+    start() {
+      this.running = true;
+    }
+    off() {
+      this.running = false;
+    }
+    driveTo(destination) {
+      if (this.running === true) {
+      console.log('Driving to ' + destination)
+      return true
+    } else if (this.running === false) {
+      return false
+      }
+    }
+    park() {
+      if (this.running === true) {
+        return false
+      } else if (this.running === false) {
+        console.log('parked!!!')
+        return true
+      }
+      }
+      pickUp() {
+        if (this.passengers.length <= this.seats.length - 1 || this.running) {
+          console.log(this.running);
+        }
+      }
 
+}
 // export the Car class //
 // this is required for the carTest.js to load this //
 module.exports = Car
--- test/carTest.js
+++ test/carTest.js
@@ -28,25 +28,66 @@ acura.sell('Lenny')
 // You're on your own from here
 
 var c1 = new Car('Honda', 'Vuzel', 2017, 'red', 7)
-
+var c2 = new Car()
 assert.strictEqual(c1.make, 'Honda', 'Constructor did not set make.')
+success()
 assert.strictEqual(c1.model, 'Vuzel', 'Constructor did not set model.')
+success()
 assert.strictEqual(c1.year, 2017, 'Constructor did not set year.')
+success()
 assert.strictEqual(c1.color, 'red', 'Constructor did not set color.')
+success()
 assert.strictEqual(c1.seats, 7, 'Constructor did not set seats.')
+success()
 assert.strictEqual(c1.owner, 'manufacturer', 'Constructor did not set owner')
+success()
 assert.strictEqual(c1.running, false, 'Constructor did not set running')
+success()
 c1.sell('prima')
 assert.strictEqual(c1.owner, 'prima', 'Constructor did set owner.')
-// console.log(c1.previousOwner)
+success()
 assert.strictEqual(c1.previousOwner[0], 'manufacturer', 'Constructor did not set previous owner')
+success()
 c1.paint('blue')
 assert.strictEqual(c1.color, 'blue', 'Constructor did not set paint method to blue')
-
+success()
+var c2 = new Car ()
+assert.strictEqual(c2.make, undefined, "car can't be instantiated, required parameters are not given" )
+success()
 c1.sell(0)
-assert.strictEqual((typeof c1.owner === 'string'), false, 'car can only be sold to a real person with name, please input a string')
-
+assert.strictEqual((typeof c1.owner === 'string'), false, 'car can only be sold to a real person with name, please input a name')
+success()
 c1.paint(false)
-assert.strictEqual((typeof c1.color === 'string'), false, 'car can only be painted with real color, please input a string')
-
+assert.strictEqual((typeof c1.color === 'string'), false, 'car can only be painted with real color, please input a color')
+success()
+c1.start()
+assert.strictEqual(c1.running, true, 'Constructor did not set start method')
+success()
+c1.off()
+assert.strictEqual(c1.running, false, 'Constuctor did not set off method')
+// Test driveTo()
+console.log('test driving to ' + 'Test!!!');
+// Test if car is not running
+c1.off()
+assert.strictEqual(c1.driveTo('off'), false, 'Should return false if car is off')
+// Test if car is running
+c1.start()
+assert.strictEqual(c1.driveTo('code'), true, 'Should return true if Car is running')
 success();
+console.log('Test park method')
+// Only if the car is not running, you should console.log parked!!.
+// Should return true if it is successful and false if it is not.
+c1.start()
+assert.strictEqual(c1.park('running'), false, 'should return false if is running')
+c1.off()
+assert.strictEqual(c1.park('Test'), true, 'should return true if car is not running')
+success();
+// console.log('pick up')
+c1.off();
+c1.pickUp();
+
+// console.log(this.running)
+// c1.off()
+// assert.strictEqual(c1.pickUp(name), false, 'car should be running')
+// c1.passengers.length > 0
+// assert.strictEqual(c1.passengers.length > 0
