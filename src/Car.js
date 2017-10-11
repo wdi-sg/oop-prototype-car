4f2d40d7b2c53097fee7b46a1aa8c2cac1a73824
Persis Ngan <persisngan@Persiss-MacBook-Pro.local>
2017-10-11 11:58:35 +0800

    commit

--- src/Car.js
+++ src/Car.js
@@ -1,19 +1,76 @@
 // Phase II here, don't require this file until you're done with Phase I
 
 class Car {
-  constructor(make, model, year, color, seats) {
+  constructor(make , model, year, color, seats, previousOwners, owner, running, passengers) {
     this.make = make
     this.model = model
     this.year = year
+    this.color = color
+    this.seats = seats
+    this.previousOwners = []
+    this.owner = `manufacturer`
+    this.running = false
+    this.passengers= []
     // TODO: add color, seats here
+
   }
 
+  sell(newOwner) {
+    this.previousOwners.push(this.owner)
+    this.owner = newOwner
+  }
+
+  paint(newColor) {
+    this.color = newColor
+  }
   // add the sell function
 
   // add the paint function
+  start() {
+    this.running = true
+  }
+
+  off() {
+    this.running = false
+  }
+
+  driveTo(destination) {
+    if (this.running === true) {
+      console.log (`driving to ${destination} `)
+      return true
+    } else return false
+  }
+
+  park() {
+    if(this.running === false) {
+      console.log(`parked!!`)
+      return true
+    } else return false
+  }
+
+  pickUp(name) {
+    if(this.running === true && this.seats > this.passengers.length) {
+      console.log(`driving to pick up ${name}`)
+      this.passengers.push(name)
+      return true
+    } else return false
+    }
+
+  dropOff(name) {
+    var i = this.passengers.indexOf(name);
+    if(this.running === true) {
+      console.log(`driving to drop off ${name}`)
+      this.passengers.splice(i, 1)
+      return true
+    } else return false
+  }
+
+  passengerCount() {
+    return parseInt(this.passengers.length)
+  }
+  }
 
 
-}
-
+module.exports = Car
 // export the Car class //
 // this is required for the carTest.js to load this //
--- src/main.js
+++ src/main.js
@@ -9,12 +9,19 @@
 
 // // Step 0 - require / import the carFn file
 
+// var Car = require('./carFn')
+// Car.sell('persis')
+// Car.paint('blue')
+// console.log(Car)
+
 // // Step 1 - Checking all properties
 // console.log the `Car` object, does it returns all the properties requested?
 
 // // Step 2 - Run `Car.sell()` function
 // `Car` object should be able to run `sell` function,
 // and updates its existing object
+
+
 
 // // Step 3 - Run `Car.paint()` function
 // `Car` object should be able to run `paint` function,
@@ -25,6 +32,14 @@
 
 // // Repeat the same steps to Phase I, but this time require the the class file
 // // instead of the the `carFn` file
+var Car = require('./Car')
+var newCar = new Car ('Honda', 'Vuzel', 2017, 'red', 7)
+console.log(newCar)
 
+var toyota = new Car ('toyota', 'rush', 2017, 'red', 7)
+console.log(toyota)
+
+toyota.sell('persis')
+console.log(toyota.previousOwners)
 
 // Phase III
