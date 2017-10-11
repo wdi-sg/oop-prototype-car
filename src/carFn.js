6e19359fa10684a8e951ca4843015932840e8368
Quek Li Yuan <QuekLiYuan@Queks-MacBook-Pro.local>
2017-10-11 11:57:19 +0800

    asn

--- src/carFn.js
+++ src/carFn.js
@@ -1,6 +1,26 @@
 // Phase I, don't require this file after you're done with Phase I
 
 // Create the object properties and function
-var Car = {}
+var Car = {
+  make: 'Honda',
+  model: 'Vezel',
+  year: 2017,
+  color: 'red',
+  seats: 7,
+  previousOwners: [],
+  owner: 'manufacturer',
+  running: false,
+  sell: function (newOwner) {
+    this.previousOwners.push(this.owner)
+    this.owner = newOwner
+  },
+  paint: function (newColor) {
+    this.color = newColor
+  }
+}
 
+// function paint(newColor) {
+//   this.color = newColor
+// }
 // Export the `Car` object
+// module.exports = Car
--- src/main.js
+++ src/main.js
@@ -8,18 +8,28 @@
 */
 
 // // Step 0 - require / import the carFn file
+var Car = require('./Car.js')
+
+var c1 = new Car('Honda', 'Vuzel', 2017, 'red', 7)
 
 // // Step 1 - Checking all properties
 // console.log the `Car` object, does it returns all the properties requested?
+console.log(Car);
 
 // // Step 2 - Run `Car.sell()` function
 // `Car` object should be able to run `sell` function,
 // and updates its existing object
 
+console.log(c1.previousOwners)
+c1.sell('me')
+console.log(c1.previousOwners)
+
 // // Step 3 - Run `Car.paint()` function
 // `Car` object should be able to run `paint` function,
 // and updates its existing object
-
+// console.log(Car.color)
+// Car.paint('green')
+// console.log(Car.color)
 
 // Phase II
 
