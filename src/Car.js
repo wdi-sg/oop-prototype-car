3ad5ea16abeb28db00342d1e5b3488695c389bbf
josephpung <goldenpummel@live.com>
2017-10-11 11:56:03 +0800

    submission

--- src/Car.js
+++ src/Car.js
@@ -1,19 +1,87 @@
 // Phase II here, don't require this file until you're done with Phase I
 
-class Car {
-  constructor(make, model, year, color, seats) {
+class Car{
+  constructor(make, model, year, color, seats){
     this.make = make
     this.model = model
     this.year = year
-    // TODO: add color, seats here
+    this.color = color
+    this.seats = seats
+    this.previousOwners = []
+    this.passengers = []
+    this.owner = "manufacturer"
+    this.running = false
+    if (this.make === undefined || this.model === undefined || this.year === undefined || this.color === undefined || this.seats === undefined){
+    throw Error("car can't be instantiated, required parameters are not given")
+    // this.checkVariable()
   }
-
-  // add the sell function
-
-  // add the paint function
-
-
+  // checkVariable(){
+  //   if (this.make === undefined || this.model === undefined || this.year === undefined || this.color === undefined || this.seats === undefined){
+  //   console.log("car can't be instantiated, required parameters are not given")
+  //   }
+  // }
+  sell(newOwner){
+    if(typeof(newOwner) !== "string"){
+      return 'car can only be sold to a real person with name, please input a string'
+    }else{
+   this.previousOwners.push(this.owner)
+   return this.owner = newOwner
+  }
+  }
+  paint(newColor){
+    if (typeof newColor !== "string"){
+      return 'car can only be painted with real color, please input a string'
+    }else{
+    return this.color = newColor
+  }
+  }
+  start(){
+    return this.running = true
+  }
+  off(){
+    return this.running = false
+  }
+  driveTo(destination){
+    if(this.running === false){
+      return false
+    }else{
+      console.log(`Driving to ${destination}`);
+      return true
+    }
+  }
+  park(){
+    if(this.running === true){
+      return false
+    }else{
+      console.log("Parked!!")
+      return true
+    }
+  }
+  passengerCount(){
+    return this.passengers.length+1
+  }
+  pickUp(name){
+    if(this.running && this.seats > this.passengers.length+1){
+      console.log(`Driving to pick up ${name}`);
+      this.passengers.push(name)
+      return true
+    }else{
+      return false
+    }
+  }
+  dropOff(name){
+    for(var i=0; i<this.passengers.length;i++){
+      if(this.passengers[i]=== name && this.running === true){
+        this.passengers.splice(i,1)
+        console.log(`Driving to drop off ${name}`);
+        return true
+      }else{
+        return false
+      }
+    }
+  }
 }
 
+module.exports = Car
 // export the Car class //
 // this is required for the carTest.js to load this //
--- test/carTest.js
+++ test/carTest.js
@@ -7,22 +7,42 @@ var success = require('./helpers/success')
 // load the Car object for
 var Car = require('../src/Car')
 // update the car instantiation below according to the test given
-var acura = new Car('Honda', 'Vuzel', 2017, 'Blue', 7)
-
+var acura = new Car('Acura', 'Integra', 1999, 'Red', 4)
 
 // starter code - testing constructor
-console.log('Testing Constructor')
+console.log('<================ Testing Constructor [1/4] ================>')
 assert.strictEqual(acura.make, 'Acura', 'Constructor did not set make.')
 assert.strictEqual(acura.model, 'Integra', 'Constructor did not set model.')
 assert.strictEqual(acura.year, 1999, 'Constructor did not set year.')
 assert.strictEqual(acura.color, 'Red', 'Constructor did not set color.')
 assert.strictEqual(acura.seats, 4, 'Constructor did not set seats.')
-
+console.log("*Constructors success*");
+console.log("<================ Testing abnormal cases [2/4] =============>");
+assert.strictEqual(acura.sell(2),  'car can only be sold to a real person with name, please input a string',"No error message")
+assert.strictEqual(acura.paint(false), 'car can only be painted with real color, please input a string',"No reject on non string")
+console.log("*Abnormal Cases success*");
+console.log("<================ Testing Phase IV [3/4] ===================>");
+assert.strictEqual(acura.start(),true,"start() did not return true")
+assert.strictEqual(acura.off(),false,"off() did not return false")
+assert.strictEqual(acura.driveTo("Somewhere"),false,"driveTo() did not return false when car is off")
+acura.start()
+assert.strictEqual(acura.driveTo("Somewhere"),true,"driveTo() did not return true when car is on")
+assert.strictEqual(acura.park(),false,"park() did not return false when car is on")
+acura.off()
+assert.strictEqual(acura.park(),true,"park() did not return true when car is off")
+console.log("*Phase IV success*");
+console.log("<=============== Testing Phase V [3/4] ===================>");
+assert.strictEqual(acura.pickUp("Bob"),false,"pickUp() did not return false when car is off")
+acura.start()
+assert.strictEqual(acura.pickUp("Bob"),true,"pickUp() did not return true when car is running")
+assert.strictEqual(acura.passengerCount(),acura.passengers.length+1,"passengerCount() did not return the correct amount of passengers")
+console.log("*Phase V success*");
+console.log("*************** ALL TESTS PASSED (4/4) *****************");
 // run the success function once you're done with a set of tests
 success()
 
 // test sell
-console.log('Testing selling a car')
-acura.sell('Lenny')
+
+
 
 // You're on your own from here
