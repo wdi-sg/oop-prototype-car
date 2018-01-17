ff343a0f3a906251417f99eee42db30e3445a61f
Speed Chan <speedchanjun@gmail.com>
2018-01-17 11:05:18 +0800

    hey

--- car.rb
+++ car.rb
@@ -14,18 +14,27 @@ class Car
   end
 
   def sell(new_owner)
-    @previous_owners.push(@owner) #pushes current owner into previous owner array
-    @owner = new_owner #sets owner to new owner
+    if new_owner.is_a? String
+      @previous_owners.push(@owner) #pushes current owner into previous owner array
+      @owner = new_owner #sets owner to new owner
+    else
+      p 'car can only be sold to a real person with name, please input a string'
+    end
   end
 
   def paint(new_color)
-    @color = new_color
+    if new_color.is_a? String
+      @color = new_color
+    else
+      p 'car can only be painted with real color, please input a string'
+    end
   end
 
 end
 
-p c1 = Car.new('Honda', 'Vuzel', 2017, 'red', 7)
-p c1.sell('prima')
-p c1.paint('blue')
+c1 = Car.new('Honda', 'Vuzel', 2017, 'red', 7)
 
-p c1
+# p c1
+# p c1.sell('prima')
+# p c1.paint('blue')
+p c1.sell(0)
